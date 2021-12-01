28f51f55f9e6ccd4e4affb1ba5225b6fe7aa258d
HonmaMeiko <17mwang2@stu.edu.cn>
2021-12-02 00:27:37 +0800

    init

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -1,10 +1,18 @@
 import { Controller, Get, Provide } from '@midwayjs/decorator';
+import axios from 'axios';
 
 @Provide()
 @Controller('/')
 export class HomeController {
   @Get('/')
   async home() {
+    axios
+      .get(
+        'https://api.mercari.jp/search_index/search?sort=score&order=desc&limit=120&keyword=anohana&status=on_sale&page=0'
+      )
+      .then(data => {
+        process.stdout.write(data.data);
+      });
     return 'Hello Midwayjs!';
   }
 }
