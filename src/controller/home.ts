d76c1ad41688b011b9d33dbf57962a02ed0a721a
HonmaMeikoDesu <whonmameikom@gmail.com>
2021-12-03 00:45:09 +0800

    guess it should be a milestone here

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -19,6 +19,7 @@ export class HomeController {
             DPoP: 'eyJ0eXAiOiJkcG9wK2p3dCIsImFsZyI6IkVTMjU2IiwiandrIjp7ImNydiI6IlAtMjU2Iiwia3R5IjoiRUMiLCJ4IjoiZlRHTFpZbFpGemkteHF1U0VyZWNwQTFxSklPRENRN1F5bV91VjNZY0JIbyIsInkiOiJmbkdMMk1XcGVFaWQtUWdmUldWdnJtVXljc3piV1pTLWI5eERJc0dpNW1BIn19.eyJpYXQiOjE2MzgzNzE4MDksImp0aSI6IjIxYjE5YzU2LTI2NjYtNGVjYS1hZjNiLTQwN2M4NDk5YjViZCIsImh0dSI6Imh0dHBzOi8vYXBpLm1lcmNhcmkuanAvc2VhcmNoX2luZGV4L3NlYXJjaCIsImh0bSI6IkdFVCIsInV1aWQiOiI2YTgwMGNmYS03MmYzLTQxZGItYTE2Zi00NGQ1YjAxYzlhNGQifQ.Y8wMdUC36scGmCqKdvT-ZvFcGukswu3pnnIfxae5ZgyUIO1U6vRmun_3M3hbMMdlp7esU7n9EexeOoF4CJamzA',
           },
           httpsAgent: agent,
+          proxy: false,
         }
       )
       .then(data => {
