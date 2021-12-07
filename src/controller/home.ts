6a4fa47077d5181038ebb063b61ecd7757dce902
HonmaMeikoDesu <whonmameikom@gmail.com>
2021-12-08 00:40:48 +0800

    code refine

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -11,7 +11,7 @@ export class HomeController {
   @Get('/')
   async home() {
     await this.proxyGet("https://api.mercari.jp/search_index/search?sort=score&order=desc&limit=120&keyword=anohana&status=on_sale&page=0", {
-      "X-Platform": "Web",
+      "X-Platform": "web",
       "DPoP": "eyJ0eXAiOiJkcG9wK2p3dCIsImFsZyI6IkVTMjU2IiwiandrIjp7ImNydiI6IlAtMjU2Iiwia3R5IjoiRUMiLCJ4IjoiZlRHTFpZbFpGemkteHF1U0VyZWNwQTFxSklPRENRN1F5bV91VjNZY0JIbyIsInkiOiJmbkdMMk1XcGVFaWQtUWdmUldWdnJtVXljc3piV1pTLWI5eERJc0dpNW1BIn19.eyJpYXQiOjE2Mzg4OTQzOTAsImp0aSI6ImU1YWYwNGJlLWNmYWUtNDcxMi04NmNjLWQyNTQwMDQ5ODAxOSIsImh0dSI6Imh0dHBzOi8vYXBpLm1lcmNhcmkuanAvc2VhcmNoX2luZGV4L3NlYXJjaCIsImh0bSI6IkdFVCIsInV1aWQiOiI2YTgwMGNmYS03MmYzLTQxZGItYTE2Zi00NGQ1YjAxYzlhNGQifQ.5M0y5JF9GoiWXcAnFcoSoEIeq1cAfvzrQm-uJarc0yAMPbZTUdLpY_BS8DaofbZnHIl4vFUnCno_OVipBqLRDA"
     }).then((data) => console.log(data));
 
