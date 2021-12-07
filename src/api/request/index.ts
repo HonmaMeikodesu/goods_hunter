b8cdc81c0feb97f7d3fed2118bbe7a0154785798
HonmaMeiko <17mwang2@stu.edu.cn>
2021-12-08 00:12:04 +0800

    add code lint infrastructure

--- src/api/request/index.ts
+++ src/api/request/index.ts
@@ -1,27 +1,25 @@
-import { IMidwayContainer, providerWrapper } from "@midwayjs/core";
-import { Configuration, App, Inject } from "@midwayjs/decorator";
-import { HttpService } from "@midwayjs/axios";
-import { AxiosRequestConfig } from "axios";
+import { IMidwayContainer, providerWrapper } from '@midwayjs/core';
+import { HttpService } from '@midwayjs/axios';
+import { AxiosRequestConfig } from 'axios';
+import { proxyInbound } from '../const';
 const HttpsProxyAgent = require('https-proxy-agent');
 
 export function proxyGet(container: IMidwayContainer) {
-    return async (url: string, headers?: AxiosRequestConfig) => {
-        const httpService = await container.getAsync<HttpService>(HttpService);
-        const agent = new HttpsProxyAgent('http://localhost:1218');
-        httpService
-            .get(
-            url,
-            {
-                ...head
-            }
-            )
-            .then(data => {
-            process.stdout.write(data.data);
-            })
-    }
+  return async (url: string, headers?: AxiosRequestConfig['headers']) => {
+    const httpService = await container.getAsync<HttpService>(HttpService);
+    const httpsAgent = new HttpsProxyAgent(proxyInbound);
+    return httpService
+      .get(url, {
+        headers,
+        httpsAgent,
+        proxy: false,
+      });
+  };
 }
 
-providerWrapper([{
-    id: "proxyGet",
+providerWrapper([
+  {
+    id: 'proxyGet',
     provider: proxyGet,
-}]);
+  },
+]);
