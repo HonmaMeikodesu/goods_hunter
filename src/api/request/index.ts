0ea4e3376e87a5c62c3eaec1f100ed52028cccb2
HonmaMeiko <17mwang2@stu.edu.cn>
2021-12-08 00:35:56 +0800

    code refine

--- src/api/request/index.ts
+++ src/api/request/index.ts
@@ -1,9 +1,12 @@
 import { IMidwayContainer, providerWrapper } from '@midwayjs/core';
 import { HttpService } from '@midwayjs/axios';
-import { AxiosRequestConfig } from 'axios';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { proxyInbound } from '../const';
 const HttpsProxyAgent = require('https-proxy-agent');
 
+export declare interface ProxyGet {
+  (url: string, headers?: Record<string, string>): Promise<AxiosResponse<any>>;
+}
 export function proxyGet(container: IMidwayContainer) {
   return async (url: string, headers?: AxiosRequestConfig['headers']) => {
     const httpService = await container.getAsync<HttpService>(HttpService);
