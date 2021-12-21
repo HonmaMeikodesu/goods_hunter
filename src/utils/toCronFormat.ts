8602e9590771de7bce84c13111b69a0d427a83b9
HonmaMeikodesu <whonmameikom@gmail.com>
2021-12-21 18:16:18 +0800

    remove duplicate logic

--- src/utils/toCronFormat.ts
+++
@@ -1,7 +0,0 @@
-const MIN = "*/5 * * * * ?"
-const MAX = "0 0 0 1 */12 ?"
-export default function toCronFormat(time: number) {
-  if (time < 300) return MIN;
-  if (time > 31536000) return MAX;
-  return "";
-}
